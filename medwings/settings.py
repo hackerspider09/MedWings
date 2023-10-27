9d7007f8519609f7f54b7a7c67f2e0266d3b9b5b
prasad <hackerspider09@gmail.com>
2023-10-27 18:46:22 +0530

    *added

--- medwings/settings.py
+++ medwings/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^8(%uuphy&yno8m)j38wp0xsp%oh9@za#f9cey&o8nacedbhud
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
