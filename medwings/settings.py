5f49764b919f29af789c356f466c7f496fd52119
prasad <hackerspider09@gmail.com>
2023-10-27 18:52:26 +0530

    dotenv

--- medwings/settings.py
+++ medwings/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^8(%uuphy&yno8m)j38wp0xsp%oh9@za#f9cey&o8nacedbhud'
+# SECRET_KEY = 'django-insecure-^8(%uuphy&yno8m)j38wp0xsp%oh9@za#f9cey&o8nacedbhud'
+SECRET_KEY = os.getenv('KEY_VARIABLE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
